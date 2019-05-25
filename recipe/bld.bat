04b779786d5084b731978e46210a673f0f9429f1
Ari Hartikainen <ari.hartikainen@aalto.fi>
2019-05-25 18:22:45 +0300

    remove files on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,13 @@ set "STAN_VER=%PKG_VERSION:~0,-2%"
 pushd pystan\\stan\\lib\\stan_math
 if errorlevel 1 exit 1
 
+SETLOCAL ENABLEDELAYEDEXPANSION
+FOR /D /R "pystan/stan/lib/stan_math/lib" %%D IN (doc) DO @IF EXIST "%%D" RD /S /Q "%%D"
+FOR /D /R "pystan/stan/lib/stan_math/lib" %%D IN (test) DO @IF EXIST "%%D" RD /S /Q "%%D"
+FOR /D /R "pystan/stan/lib/stan_math/lib" gtest IN (%%D) DO @IF EXIST "%%D" RD /S /Q "%%D"
+FOR /D /R "pystan/stan/lib/stan_math/lib" cpplist IN (%%D) DO @IF EXIST "%%D" RD /S /Q "%%D"
+
+
 rd /q /s doc doxygen make test lib\\cpplist_* lib\\gtest_*
 if errorlevel 1 exit 1
 
