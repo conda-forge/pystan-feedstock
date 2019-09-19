f13f953526258cd65780b74bcc6471e5d429f7fa
Matthew R. Becker <beckermr@users.noreply.github.com>
2019-09-18 23:09:42 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,6 @@
 set -ex
 
 STAN_VER=${PKG_VERSION:0:${#PKG_VERSION}-2}
-INSTALL_LOG=install.log
 
 # force recreate cython files
 rm -rf pystan/*.cpp
@@ -13,30 +12,4 @@ pushd pystan/stan/lib/stan_math
 rm -fr doc doxygen make test lib/cpplist_* lib/gtest_*
 popd
 
-# Log everything to a file to avoid reaching max output limit in travis.
-touch $INSTALL_LOG
-
-function dump_output() {
-  let N=${1:-"10"}
-  echo "Tailing the last $N lines of output"
-  tail -${N} $INSTALL_LOG
-}
-
-function error_handler() {
-  echo "ERROR: An error was encountered in the build"
-  dump_output 500
-  exit 1
-}
-
-trap 'error_handler' ERR
-
-bash -c "while true; do echo === \$(date) === building ...; sleep 60; done" &
-PING_LOOP_PID=$!
-
-python setup.py install -q --single-version-externally-managed --record=record.txt \
-  >> $INSTALL_LOG 2>&1
-
-# Build finished OK
-dump_output
-
-kill $PING_LOOP_PID
+${PYTHON} -m pip install . -vv
