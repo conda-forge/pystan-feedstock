4b82e6959afa8d9779613559be6c7c019b22b53e
David Hirschfeld <david.hirschfeld@stanwell.com>
2021-05-12 20:14:45 +1000

    Update build scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,21 +1,4 @@
 set "STAN_VER=%PKG_VERSION:~0,-2%"
 
-# force recreate cython files
-rm -rf pystan/*.cpp
-
-pushd pystan\\stan\\lib\\stan_math
-if errorlevel 1 exit 1
-
-SETLOCAL ENABLEDELAYEDEXPANSION
-FOR /D /R "pystan/stan/lib/stan_math/lib" %%D IN (doc) DO @IF EXIST "%%D" RD /S /Q "%%D"
-FOR /D /R "pystan/stan/lib/stan_math/lib" %%D IN (test) DO @IF EXIST "%%D" RD /S /Q "%%D"
-
-
-rd /q /s doc doxygen make test lib\\cpplist_* lib\\gtest_*
-if errorlevel 1 exit 1
-
-popd
-if errorlevel 1 exit 1
-
 python setup.py install -q --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,4 @@ set -ex
 
 STAN_VER=${PKG_VERSION:0:${#PKG_VERSION}-2}
 
-# force recreate cython files
-rm -rf pystan/*.cpp
-
-# These folders are not needed in the build and we don't want to include them
-# in the package. This should be cleaned via MANIFEST.in.
-pushd pystan/stan/lib/stan_math
-rm -fr doc doxygen make test lib/cpplist_* lib/gtest_*
-popd
-
 ${PYTHON} -m pip install . -vv
