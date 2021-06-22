560fdc2dd0d0fb1c81c059760e4dd8c85680a832
oblute <olivia.blute@gmail.com>
2021-06-22 16:20:32 -0400

    see if there was a error

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -5,21 +5,21 @@ os.system("gcc --version")
 
 schools_code = """
 data {
-    int<lower=0> J;         // number of schools
-    real y[J];              // estimated treatment effects
-    real<lower=0> sigma[J]; // standard error of effect estimates
+  int<lower=0> J;         // number of schools
+  real y[J];              // estimated treatment effects
+  real<lower=0> sigma[J]; // standard error of effect estimates
 }
 parameters {
-    real mu;                // population treatment effect
-    real<lower=0> tau;      // standard deviation in treatment effects
-    vector[J] eta;          // unscaled deviation from mu by school
+  real mu;                // population treatment effect
+  real<lower=0> tau;      // standard deviation in treatment effects
+  vector[J] eta;          // unscaled deviation from mu by school
 }
 transformed parameters {
-    vector[J] theta = mu + tau * eta;        // school treatment effects
+  vector[J] theta = mu + tau * eta;        // school treatment effects
 }
 model {
-    target += normal_lpdf(eta | 0, 1);       // prior log-density
-    target += normal_lpdf(y | theta, sigma); // log-likelihood
+  target += normal_lpdf(eta | 0, 1);       // prior log-density
+  target += normal_lpdf(y | theta, sigma); // log-likelihood
 }
 """
 
