6a0f1dd6fa1ffde94619b179a7a80ea9b3dcae6b
Daniel Cunningham <52785894+d-cunningham@users.noreply.github.com>
2021-09-02 18:59:25 -0400

    removed stan_math

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,6 @@ rm -rf pystan/*.cpp
 
 # These folders are not needed in the build and we don't want to include them
 # in the package. This should be cleaned via MANIFEST.in.
-pushd pystan/stan/lib/stan_math
 rm -fr doc doxygen make test lib/cpplist_* lib/gtest_*
 popd
 
