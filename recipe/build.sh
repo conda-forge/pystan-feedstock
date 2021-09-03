d09d2945d0a94fbe758855b1432fb25d113b9ef1
Daniel Cunningham <52785894+d-cunningham@users.noreply.github.com>
2021-09-03 00:28:43 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
-STAN_VER=${PKG_VERSION:0:${#PKG_VERSION}-2}
+STAN_VER=${PKG_VERSION:0:${#PKG_VERSION}-3}
+
+# force recreate cython files
+rm -rf pystan/*.cpp
 
 ${PYTHON} -m pip install . -vv
