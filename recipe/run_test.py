80158361ecd604faf878d417cf7e21cf9343483c
Daniel Cunningham <52785894+d-cunningham@users.noreply.github.com>
2021-09-02 22:53:01 -0400

    Update run_test.py

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -24,8 +24,7 @@ schools_data = {"J": 8,
                 "y": [28,  8, -3,  7, -1,  1, 18, 12],
                 "sigma": [15, 10, 16, 11,  9, 11, 10, 18]}
 
-posterior = stan.build(schools_code, data=schools_data, random_seed=1)
+posterior = stan.build(schools_code, data=schools_data)
 fit = posterior.sample(num_chains=4, num_samples=1000)
-
-fit["eta"]
-print(fit)
+eta = fit["eta"]  # array with shape (8, 4000)
+df = fit.to_frame()  # pandas `DataFrame`
