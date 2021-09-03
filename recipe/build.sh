978612de6791e9eb659ccc5d388360fcc132a5db
Daniel Cunningham <52785894+d-cunningham@users.noreply.github.com>
2021-09-02 20:39:50 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,9 +6,4 @@ STAN_VER=${PKG_VERSION:0:${#PKG_VERSION}-2}
 # force recreate cython files
 rm -rf pystan/*.cpp
 
-# These folders are not needed in the build and we don't want to include them
-# in the package. This should be cleaned via MANIFEST.in.
-rm -fr doc doxygen make test lib/cpplist_* lib/gtest_*
-popd
-
 ${PYTHON} -m pip install . -vv
